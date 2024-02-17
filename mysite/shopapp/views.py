e59f7ea3c1d37b0d823feb9ff8c382b0f4e36fb9
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-17 20:52:29 +0300

    Добавил отображение данных в шаблоне через context

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -2,8 +2,14 @@ from django.http import HttpRequest,HttpResponse
 from django.shortcuts import render
 from timeit import default_timer
 def shop_index(request: HttpRequest):
+    devices = [
+        ("Laptop",2900),
+        ("Smartphone",4900),
+        ("Television",6999)
+    ]
     context = {
-        "timmer": default_timer
+        "timmer": default_timer,
+        "devices": devices
     }
     return render(request,'shopapp/shop-index.html',context = context)
 # Create your views here.
