f716f3ae2e3acbe95073126c66095a386e77b763
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-17 20:45:31 +0300

    Добавил в приложение два шаблона, из которых один базовый. Второй является его наследником. Через render отображаем шаблон вместе с необходимой информацией

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpRequest,HttpResponse
 from django.shortcuts import render
+from timeit import default_timer
 def shop_index(request: HttpRequest):
-    return HttpResponse("<b>Hellow World!</b>")
+    context = {
+        "timmer": default_timer
+    }
+    return render(request,'shopapp/shop-index.html',context = context)
 # Create your views here.
