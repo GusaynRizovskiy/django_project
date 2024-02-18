788bbbfdc533bbc20672ba350cacff0c83f6438c
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-18 17:09:04 +0300

    Создали связь один ко многим между пользователем и заказом, а также связь многие ко многим между продуктами и заказами. Отобразили все с помощью шаблонов.

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import shop_index,groups,products
+from .views import shop_index,groups,products,orders
 
 urlpatterns = [
     path('', shop_index,name = 'shop_index'),
     path('groups/',groups,name = 'groups_list'),
-    path('products',products,name = 'products_list')
+    path('products',products,name = 'products_list'),
+    path('orders/', orders, name = 'orders_list')
 ]
