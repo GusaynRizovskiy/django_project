1aa9289114f5ada470198605bc026c0b99f2048c
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-19 21:06:33 +0300

    Добавил class Order в admin модель. Прописал связи один ко многим и многие ко многим. Отображение в django админке корректное, использовал методы get_query_set и user_verbose для подгружение связанных атрибутов объекта. Добавил Inline модели для отображения связи многие ко многим

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from shopapp.models import Product
+from shopapp.models import Product,Order
 
-
+class ProductInline(admin.TabularInline):
+    model = Product.orders.through
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline
+    ]
     list_display = "pk", "name", "description_short", "price", "discount"
     list_display_links = "pk","name"
     ordering = 'pk', 'name'
@@ -13,4 +17,17 @@ class ProductAdmin(admin.ModelAdmin):
         if len(obj.description) < 48:
             return obj.description
         return obj.description[:48] + '...'
+
+class OrderInline(admin.TabularInline):
+    model = Order.products.through
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline
+    ]
+    list_display = "delivery_address","promocode","created_at","user_verbose"
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+    def user_verbose(self,obj:Order) -> str:
+        return obj.user.first_name or Order.user.username
