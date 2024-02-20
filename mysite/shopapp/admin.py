0b60c06f63fc8d54ac134e5557a0dc2916c0986e
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-20 21:34:52 +0300

    Попрактиковался еще раз для лучшего закрепления материала.

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
 
 from shopapp.models import Product, Order
-
+@admin.action(description='Archived products')
+def mark_archived(modeladmin: admin.ModelAdmin,request: HttpRequest,queryset:QuerySet):
+    queryset.update(archived=True)
+@admin.action(description='Unarchived products')
+def mark_unarchived(modeladmin: admin.ModelAdmin,request: HttpRequest,queryset:QuerySet):
+    queryset.update(archived=False)
 
 class ProductInline(admin.TabularInline):
     model = Product.orders.through
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = [
+        mark_archived,
+        mark_unarchived,
+    ]
     inlines = [
         ProductInline,
     ]
-    list_display = 'name','description','price','discount'
+    list_display = 'name','description','price','discount','archived',
     list_display_links = 'name',
     ordering =  'id',
     search_fields = ['name','description']
@@ -22,9 +33,9 @@ class ProductAdmin(admin.ModelAdmin):
             'fields': ('price','discount'),
             'classes': ('collapse','wide')
         }),
-        ("Extra information", {
-            'fields': ('archived'),
-            'description': ('Extra information for you.',),
+        ("Extra options", {
+            'fields': ('archived',),
+            'description': ("You can archived this product or don't do this"),
         })
     ]
     def short_description(self,obj:Product):
