13097c5728486f8b0d780d9b5c98912fb416cde7
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-20 20:34:00 +0300

    Добавил новый функционал в django admin. Теперь может выбирать ряд действий на панели. Кроме того создали admin mixin.

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
-
+from django.db.models import QuerySet
+from django.http import HttpRequest
+from .admin_mixins import ExportAsCSVMixin
 from shopapp.models import Product,Order
 
 class ProductInline(admin.TabularInline):
     model = Product.orders.through
+@admin.action(description="Archive products")
+def mark_archived(modeladmin: admin.ModelAdmin,request: HttpRequest, queryset: QuerySet):
+    queryset.update(archived = True)
+@admin.action(description="Unarchive products")
+def mark_unarchived(modeladmin: admin.ModelAdmin,request: HttpRequest, queryset: QuerySet):
+    queryset.update(archived = False)
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin,ExportAsCSVMixin):
+    actions = [
+        mark_archived,
+        mark_unarchived,
+        "export_csv",
+    ]
     inlines = [
         ProductInline
     ]
-    list_display = "pk", "name", "description_short", "price", "discount"
+    list_display = "pk", "name", "description_short", "price", "discount",'archived'
     list_display_links = "pk","name"
     ordering = 'pk', 'name'
     search_fields = 'name','description'
@@ -21,12 +34,16 @@ class ProductAdmin(admin.ModelAdmin):
             'fields': ('price',"discount"),
             'classes': ('collapse',"wide"),
         }),
-
+        ("Extra options", {
+            'fields': ('archived',),
+            'description': ("You can archived this product or don't do this"),
+        })
     ]
     def description_short(self,obj: Product) -> str:
         if len(obj.description) < 48:
             return obj.description
         return obj.description[:48] + '...'
+
 
 class OrderInline(admin.TabularInline):
     model = Order.products.through
