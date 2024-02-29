9b4ae4e1f4396a561c869e9ee9f4d3834daa75f8
GusaynRizovskiy <makkks200426013@mail.ru>
2024-02-29 21:39:09 +0300

    Выполнил практические задание связанные с проверкой размера файла и ограничения числа запросов по времени с одного Ip адреса. Изучил MiddleWare и концепции MVC, MTV. Изучил POST and GET запросы.

--- mysite/request_app_data/views.py
+++ mysite/request_app_data/views.py
@@ -15,11 +15,16 @@ def process_get_view(request: HttpRequest) -> HttpResponse:
     return render(request,'request_app_data/request-query-params.html', context)
 def user_form(request: HttpRequest)->HttpResponse:
     return render(request,'request_app_data/request-bio-form.html')
-def handle_file_upload(request: HttpRequest)->HttpResponse:
+def handle_file_upload_with_size(request: HttpRequest)->HttpResponse:
     if request.method == "FILES" and request.POST.get("myfile"):
         myfile = request.FILES["myfile"]
         fs = FileSystemStorage()
         file_name = fs.save(myfile.name,myfile)
-        print("saved file",file_name)
-
+        file_size = fs.size(file_name)
+        if file_size>1048576:
+            fs.delete(file_name)
+            print("file is deleted",file_name)
+            return render(request,"request_app_data/error_size_file.html")
+        else:
+            print("file is saved with name ",file_name)
     return render(request,'request_app_data/file-upload.html')
