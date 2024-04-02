3425293be70fdc8031f263cded80a801d29084e2
GusaynRizovskiy <makkks200426013@mail.ru>
2024-04-02 17:40:53 +0300

    выа

--- mysite/myauth/tests.py
+++ mysite/myauth/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
+
+class GetCoockieViewTestCase(TestCase):
+    def test_get_coockie_view(self):
+        response = self.client.get(reverse("myauth:get_coockie"))
+        self.assertContains(response,"Coockie set")
 # Create your tests here.
--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate,login
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
 #
@@ -16,3 +16,11 @@ from django.shortcuts import render, redirect
 #         return redirect("/admin/")
 #     return render(request,"myauth/login.html", {"error":"Invalid username or password"})
 # # Create your views here.
+
+def get_coockie(request:HttpRequest):
+    value = request.COOKIES.get("fizz","default_name")
+    return HttpResponse(f"Coockie value: {value}")
+def set_coockie(request:HttpRequest):
+    response = HttpResponse("Set Coockie")
+    response.set_cookie("fizz","buzz")
+    return response
