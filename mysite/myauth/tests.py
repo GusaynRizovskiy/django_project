8729de7afd96e7059a35355945e76e52336fdde2
GusaynRizovskiy <makkks200426013@mail.ru>
2024-04-03 21:42:15 +0300

    Одна из тренировок

--- mysite/myauth/tests.py
+++ mysite/myauth/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class GetCoockieViewTestCase(TestCase):
+    def test_get_coockie(self):
+        response = self.client.get(reverse("myauth:get_coockie"))
+        self.assertContains(response,"Coockie get")
+
+class FooBarViewTestCase(TestCase):
+    def test_foo_bar(self):
+        response = self.client.get(reverse("myauth:foo-bar"))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response.headers['content-type'],'application/json')
+        expected_date = {"putin":"good","baiden":"evil"}
+        self.assertEqual(response.content,expected_date)
--- mysite/myauth/urls.py
+++ mysite/myauth/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
+from .views import get_coockie,set_coockie,FooBarView
 
 
 app_name = 'myauth'
@@ -8,4 +9,7 @@ urlpatterns = [
         template_name='myauth/login.html',
         redirect_authenticated_user=True,
     ),name="login"),
+    path('get_coockie/',get_coockie,name='get_coockie'),
+    path('set_coockie/',set_coockie,name='set_coockie'),
+    path('foobar/',FooBarView.as_view(),name='foo-bar'),
 ]
