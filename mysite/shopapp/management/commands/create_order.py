439be741a73870f63130895a3dc8516e4d3c081e
GusaynRizovskiy <makkks200426013@mail.ru>
2024-05-18 15:40:24 +0300

    Ознакомился с механизмом транзакций в базе данных

--- mysite/shopapp/management/commands/create_order.py
+++ mysite/shopapp/management/commands/create_order.py
@@ -1,19 +1,26 @@
+from typing import Sequence
+
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand
-
-from shopapp.models import Order
+from django.db import transaction
+from shopapp.models import Order, Product
 
 
 class Command(BaseCommand):
     """
     Create order
     """
+    @transaction.atomic
     def handle(self, *args, **options):
-        self.stdout.write("Create order")
+        self.stdout.write("Create order with product")
         user = User.objects.get(username = 'admin')
-        order = Order.objects.get_or_create(
-            delivery_address = "Vorobieva 3",
-            promocode = "FK934QA",
-            user = user
+        products: Sequence[Product] = Product.objects.all()
+        order, created = Order.objects.get_or_create(
+            delivery_address = "Vorolakieva 33",
+            promocode = "prommo2",
+            user = user,
         )
+        for product in products:
+            order.products.add(product)
+        order.save()
         self.stdout.write(f"Created order{order}")
